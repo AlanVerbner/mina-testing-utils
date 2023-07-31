0967def8207b6e79719f46dbae62256383aa55cb
Gonzalo Bustos <gonzalo.bustos@ar-it23280.fibertel.com.ar>
2023-07-31 12:56:14 -0300

    feat: attempt to implement compile command

--- scripts/mina-cli.js
+++ scripts/mina-cli.js
@@ -1,5 +1,6 @@
 const repl = require('repl');
 const { Mina, PrivateKey } = require("snarkyjs");
+// const { TestZkApp } = require("../src/contracts/test.mts");
 
 let mina = Mina.LocalBlockchain({ proofsEnabled: false });
 Mina.setActiveInstance(mina);
@@ -21,6 +22,13 @@ const myREPL = repl.start({
   prompt: 'mina > ',
   ignoreUndefined: true,
   replMode: repl.REPL_MODE_STRICT,
+});
+
+myREPL.defineCommand('compile', () => {
+  // const c = TestZkApp.compile();
+  // console.log("compiled: ", c);
+  console.log("This command should aim to compile a specific contract or all contracts and display their constraints");
+  myREPL.displayPrompt();
 });
 
 // Assign the predefined variables to the context of the REPL
